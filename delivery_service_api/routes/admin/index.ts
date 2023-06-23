c80ad13f25cf0160df6e0528a0b65578adbe7b89
Aung Hein <aungcho2003@gmail.com>
2023-06-24 00:20:59 +0630

    add admin auth middleware

--- delivery_service_api/routes/admin/index.ts
+++ delivery_service_api/routes/admin/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import authRoute from "./auth/views";
+import { authorizeUser } from "./middlewares";
 
 const route = express.Router();
 
 route.use("/auth", authRoute);
+
+route.use(authorizeUser);
 
 export default route;
---
+++ delivery_service_api/routes/admin/middlewares.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function authorizeUser(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token = req.headers["authorization"]?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ success: false, msg: "Token missing!" });
+  }
+
+  try {
+    const decode: any = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!);
+
+    const code = decode?.code;
+    const hashed_password = decode?.hashed_password;
+
+    if (!code || !hashed_password) {
+      return res.status(403).json({ success: false, msg: "re-auth" });
+    }
+
+    const dbAdmin = await prisma.admin.findUnique({ where: { code } });
+
+    if (!dbAdmin) {
+      return res.status(403).json({ success: false, msg: "re-auth" });
+    }
+
+    const { hashed_password: hash_pwd_db } = dbAdmin;
+
+    if (hashed_password !== hash_pwd_db) {
+      return res.status(403).json({ success: false, msg: "re-auth" });
+    }
+    next();
+  } catch (err) {
+    return res.status(403).json({ success: false, msg: "re-auth" });
+  }
+}
